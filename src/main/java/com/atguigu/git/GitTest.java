0a9d9789ef16b77eadc7fa2fb89d5b292de76456
j <123@qq.com>
2023-06-25 10:56:14 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author:jzh
+ * @Description:
+ * @create:2023/6/25 10:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+}
