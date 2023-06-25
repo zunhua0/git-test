3f65216f7ca5efc114c364b8e318732f44b08132
zunhua0 <128290136+zunhua0@users.noreply.github.com>
2023-06-25 11:41:54 +0800

    Update GitTest.java
    pull

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello, master!");
         System.out.println("hello, hot-fix!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
